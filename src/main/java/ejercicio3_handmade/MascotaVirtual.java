70f578de81f07e9ce1a06fd0f480d0bc2353d688
Freites Joaquin Valentino - 408476 <joautn@gmail.com>
2026-08-27 20:29:40 -0300

    Se agregaron algunos puntos más. Aún quedan pendientes otros.

--- src/main/java/ejercicio3_handmade/MascotaVirtual.java
+++ src/main/java/ejercicio3_handmade/MascotaVirtual.java
@@ -10,27 +10,20 @@ public class MascotaVirtual {
     private boolean durmiendo;
     private int cant_ingestas_cons;
     private boolean viva;
+    private int cant_actividades_consecutivas;
+
     public MascotaVirtual(){
 
     }
-    public MascotaVirtual(int energia, int humor, boolean durmiendo,  int cant_ingestas_cons,  boolean viva) {
+
+    public MascotaVirtual(int energia, int humor, boolean durmiendo,  int cant_ingestas_cons,  boolean viva, int cant_actividades_consecutivas) {
         this.energia = energia;
         this.humor = humor;
         this.durmiendo = durmiendo;
         this.cant_ingestas_cons = cant_ingestas_cons;
         this.viva = viva;
+        this.cant_actividades_consecutivas = cant_actividades_consecutivas;
     }
-
-   /* private void evaluarEstado(){
-        if (this.energia <= 0){
-            this.energia = 0;
-            this.viva = false;
-        }
-        if ( this.humor > 5){
-            this.humor = 5;
-        }
-    }
-    */
 
     // DE INGESTA
 
@@ -39,10 +32,15 @@ public boolean comer(){
         // A partir de la 3 ingesta consecutiva el nivel de humor
         // comienza a decrementar en 1 por cada ingesta.
         this.cant_ingestas_cons++;
+        this.cant_actividades_consecutivas = 0;
         if (cant_ingestas_cons >= 3){
             this.humor--;
         } else{
             this.humor++;
+        }
+        //  Si la mascota realiza 5 ingestas consecutivas muere de empacho.
+        if (this.cant_ingestas_cons == 5){
+            this.viva = false;
         }
         // incrementa la energía (que es un número entre 0 y 100 unidades)
         // en 10% de la energía que tiene la mascota y incrementa el humor en 1 nivel.
@@ -54,10 +52,16 @@ public boolean comer(){
     public boolean beber(){
         // A partir de la 3 ingesta consecutiva el nivel de humor
         // comienza a decrementar en 1 por cada ingesta.
+        this.cant_ingestas_cons++;
+        this.cant_actividades_consecutivas = 0;
         if (cant_ingestas_cons >= 3){
             this.humor--;
         } else{
             this.humor++;
+        }
+        //  Si la mascota realiza 5 ingestas consecutivas muere de empacho.
+        if (this.cant_ingestas_cons == 5){
+            this.viva = false;
         }
 
         //incrementa la energía (que es un número entre 0 y 100 unidades)
@@ -72,6 +76,10 @@ public boolean comer(){
     public boolean correr(){
         //decrementa la energía en un 35% de la energía que tiene la mascota.
         // Y decrementa el humor en 2 niveles.
+        this.cant_ingestas_cons = 0;
+        if (this.cant_actividades_consecutivas == 3){
+            this.durmiendo = true;
+        }
         double decremento = (double) (this.energia * 0.35);
         this.energia -= decremento;
         int decremento_humor = 2;
@@ -82,6 +90,10 @@ public boolean comer(){
     public boolean saltar(){
         //decrementa la energía en un 15% de la energía que tiene la mascota.
         // Y decrementa el humor en 2 niveles.
+        this.cant_ingestas_cons = 0;
+        if (this.cant_actividades_consecutivas == 3) {
+            this.durmiendo = true;
+        }
         double decremento = (double) (this.energia * 0.15);
         this.energia -= decremento;
         int decremento_humor = 2;
@@ -93,6 +105,8 @@ public boolean comer(){
     public boolean dormir(){
         //la mascota pasa a estado durmiendo y no responde a ningún otro comportamiento excepto despertar.
         // Además la energía se incrementa en 25 unidades y el humor en 2 niveles.
+        this.cant_ingestas_cons = 0;
+        this.cant_actividades_consecutivas = 0;
         this.durmiendo = true;
         this.energia += 25;
         this.humor +=  2;
@@ -102,21 +116,20 @@ public boolean comer(){
     public boolean despertar(){
         // la mascota pasa a estado despierta y comienza a responder a los demás comportamientos.
         // Además el humor se decrementa en un nivel.
+        this.cant_ingestas_cons = 0;
+        this.cant_actividades_consecutivas = 0;
+        if (this.energia == 0){
+            this.viva = false;
+        }
         this.durmiendo = false;
         this.humor -= 1;
         return true;
     }
 }
 /*
-Por otro lado se deben respetar las siguientes reglas adicionales para todos los comportamientos en general:
-2 - Cuando la energía llega a 0 la mascota muere de cansada.
-3 - Si la mascota realiza 5 ingestas consecutivas muere de empacho.
-4 - Si la mascota realiza 3 actividades consecutivas se empaca y se duerme.
-
 Agregar además el comportamiento toString que devuelva una representación de cadena de la mascota incluyendo su nombre, unidades de energía, nivel de alegría, si duerme y si vive.
 Notas:
 1. la energía no puede superar 100 unidades, es decir si está en 100 no aumenta y si baja de cero la mascota muere y ya no responde más a ningún comportamiento.
-2. el nivel de alegría fluctúa entre 1 muuuuy enojado y 5 chocho y si la alegría llega a 0 la mascota se va a dormir por propia iniciativa.
 3. cuando la mascota está en un estado que no responde a cierto comportamiento el mismo debe retornar false y si el comportamiento se pudo realizar correctamente retorna true.
 
  */
